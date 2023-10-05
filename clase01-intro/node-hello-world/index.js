5fea28948a07b0c1b612f3500a11a0da37fe4c35
doomling <bethlejem@gmail.com>
2023-10-05 11:38:21 -0300

    feat/ejemplos clase 01

---
+++ clase01-intro/node-hello-world/index.js
@@ -0,0 +1,8 @@
+// Defino la función sayHello que acepta como parámetro un nombre
+function sayHello(name) {
+  // imprimo en pantalla
+  console.log(`Hola ${name}`);
+}
+
+// ejecuto la función
+sayHello("mundo");
